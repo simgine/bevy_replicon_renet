2ce2787b9af885934344e07c7af6e60c5473cafb
Hennadii Chernyshchyk <genaloner@gmail.com>
2025-10-05 19:42:13 +0300

    Handle jumping between `ClientState::Connected` states

--- src/client.rs
+++ src/client.rs
@@ -20,10 +20,11 @@ impl Plugin for RepliconRenetClientPlugin {
             .add_systems(
                 PreUpdate,
                 (
-                    set_connecting.run_if(resource_added::<RenetClient>),
-                    set_connected.run_if(
-                        in_state(ClientState::Connecting).and(bevy_renet::client_just_connected),
-                    ),
+                    (
+                        set_connecting.run_if(resource_added::<RenetClient>),
+                        set_connected.run_if(bevy_renet::client_just_connected),
+                    )
+                        .chain(), // Make connected take precedence if the inserted resource is already connected.
                     set_disconnected.run_if(bevy_renet::client_just_disconnected),
                     receive_packets.run_if(bevy_renet::client_connected),
                 )
